437936084d9eb2c4943d55f21cc7edf6f4c62fae
Realsusu <syt90@163.com>
2018-07-24 00:05:06 +0800

    homepractice
    123

--- 0723practice/0723practice/Program.cs
+++ 0723practice/0723practice/Program.cs
@@ -10,6 +10,9 @@ namespace _0723practice
     {
         static void Main(string[] args)
         {
+
+            //No.1
+            /*
             const double Pi= 3.1415;
             double r = 1.5,h=3.6;
             double c, s, v;
@@ -19,7 +22,62 @@ namespace _0723practice
             Console.WriteLine("圆的周长是：{0:f2}", c);
             Console.WriteLine("圆的面积是：{0:0.00}", s);
             Console.WriteLine($"圆的体积是:{v:f2}");
-            Console.ReadLine();
+            */
+
+            //No.2
+            /*
+            const double Pi = 3.1414;
+            double r = 1.2, h = 1.5;
+            double v;
+            v = Pi * r * r * h;
+            Console.Write("体积为：");
+            Console.Write(v);
+            */
+
+
+            //No.3
+
+            /*
+            char ch;
+            Console.WriteLine("Enter a letter:");
+            ch = char.Parse(Console.ReadLine());
+            if (ch >= 'A' && ch <= 'Z')
+            {
+                ch = (char)(ch + 32);
+            }
+            Console.WriteLine(ch);
+            */
+
+
+            /*
+            float x, y, z, temp;
+            Console.WriteLine("请输入第一个数：");
+            x = float.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个数：");
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第三个数：");
+            z = float.Parse(Console.ReadLine());
+            temp = x <= y ? x : y;
+            temp = temp <= z ? temp : z;
+            Console.WriteLine($"最小的数为{temp}");
+            */
+
+            /*
+            float x, y, z, temp;
+            Console.WriteLine("请输入第一个数：");
+            x = float.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个数：");
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第三个数：");
+            z = float.Parse(Console.ReadLine());
+            temp = x >= y ? x : y;
+            temp = temp >= z ? temp : z;
+            Console.WriteLine($"最大的数为{temp}");
+            */
+
+
+
+            Console.ReadKey();
 
         }
     }
